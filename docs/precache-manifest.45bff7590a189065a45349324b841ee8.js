c30ec9dc14b8a921ead60576742d66012de75935
Ad <adunreal@gmail.com>
2018-12-31 15:02:31 +0530

    Init commit

---
+++ docs/precache-manifest.45bff7590a189065a45349324b841ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6542b32ba7c55e28410d",
+    "url": "/static/js/main.6542b32b.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "6542b32ba7c55e28410d",
+    "url": "/static/css/main.fbc9143b.chunk.css"
+  },
+  {
+    "revision": "40b1bae64b7b24595b064c72506fdf4d",
+    "url": "/index.html"
+  }
+];
